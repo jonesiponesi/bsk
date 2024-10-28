22ed7adf789cf81b48d1a10aacb9d0cc7ca0eb9f
jonesiponesi <jonnaliu@student.oulu.fi>
2024-10-28 17:50:21 +0200

    US3 - Test calculate score with strike

--- bowling.py
+++ bowling.py
@@ -20,12 +20,19 @@ class BowlingGame:
     def calculate_score(self) -> int:
         score = 0
         is_spare = False
+        is_strike = False
         for frame in self._frames:
+            if is_strike:
+                score += frame.get_first_throw()
+                score += frame.get_second_throw()
+                is_strike = False
             if is_spare:
                 score += frame.get_first_throw()
                 is_spare = False
             if frame.is_spare():
                 is_spare = True
+            if frame.is_strike():
+                is_strike = True
             score += frame.score()
         return score
 
